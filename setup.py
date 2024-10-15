2fbe8c37878bbd587e437f2861eb913792e7d395
Ashkan Rafiee <35492414+AshkanRafiee@users.noreply.github.com>
2024-10-15 12:25:03 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1.0',
     author='Ashkan Rafiee',
     author_email='ashkanrafiee-pypi.cheer081@passmail.net',
-    description='A policy validation tool using OpenAI GPT',
+    description='A policy validation tool using OpenAI Models',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ashkanrafiee/policy_validator',
@@ -23,6 +23,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     keywords=['openai', 'GPT', 'policy validation', 'content moderation']
 )
